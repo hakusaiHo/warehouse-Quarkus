58a52e62aff3d5d9a4ee6084268ccbf9e21b575a
hakusaiHo <a88666089@gmail.com>
2024-04-23 18:14:09 +0800

    feat: 新增儲區儲位管理的model
    新增儲區儲位管理的model

---
+++ src/main/java/wareHouse/Area/Model/ItemAreaList.java
@@ -0,0 +1,14 @@
+package wareHouse.Area.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemAreaList {
+    public String branch;
+    public Integer itemId;
+    public String preferSaveArea;
+    public String description1;
+    public String itemNumber;
+}
