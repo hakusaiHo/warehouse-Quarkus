6009a78ee0185b0c52fd12cadd1f3b3bc0b17066
hakusaiHo <a88666089@gmail.com>
2024-04-23 19:48:31 +0800

    fix: 修改料品主檔的對應欄位
    修改料品主檔的對應欄位

--- src/main/java/wareHouse/Master/Dao/ItemMaster/ItemMaster.java
+++ src/main/java/wareHouse/Master/Dao/ItemMaster/ItemMaster.java
@@ -15,6 +15,7 @@ import wareHouse.Master.AuditInfoVersionBaseEntity;
 @Table(name = "KTST_ITEM_MASTER")
 @IdClass(ItemMasterPK.class)
 public class ItemMaster extends AuditInfoVersionBaseEntity {
+
     @Id
     @Column(name = "item_id")
     public Integer itemId;
@@ -28,45 +29,13 @@ public class ItemMaster extends AuditInfoVersionBaseEntity {
     @Column(name = "description2", length = 30)
     public String description2 = "";
 
-    @Column(name = "stock_days", length = 6)
-    public Integer stockDays = 0;
+    @Column(name = "ALLERGEN_MARK")
+    public Boolean allergenMark;
 
-    @Column(name = "primary_uom", length = 2)
-    public String primaryUom = "";
+    @Column(name = "MUSLIM_MARK")
+    public Boolean muslimMark;
 
-    @Column(name = "secondary_uom", length = 2)
-    public String secondaryUom = "";
+    @Column(name = "SALES_CATEGORY_CODE", length = 3)
+    public String categoryCode;
 
-    @Column(name = "purchase_uom", length = 2)
-    public String purchaseUom = "";
-
-    @Column(name = "pricing_uom", length = 2)
-    public String pricingUom = "";
-
-    @Column(name = "shipping_uom", length = 2)
-    public String shippingUom = "";
-
-    @Column(name = "production_uom", length = 2)
-    public String productionUom = "";
-
-    @Column(name = "allocation_uom", length = 2)
-    public String allocationUom = "";
-
-    @Column(name = "weight_uom", length = 2)
-    public String weightUom = "";
-
-    @Column(name = "volume_uom", length = 2)
-    public String volumeUom = "";
-
-    @Column(name = "glcategory", length = 4)
-    public String glCategory = "";
-
-    @Column(name = "stocking_type", length = 1)
-    public String stockingType = "";
-
-    @Column(name = "buyer")
-    public Long buyer;
-
-    @Column(name = "address_number_planner")
-    public Long addressNumberPlanner;
 }
