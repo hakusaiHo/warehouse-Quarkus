247e4761abc974958cc4a5d6e5fb48b0e586a5c6
hakusaiHo <a88666089@gmail.com>
2024-04-24 21:02:52 +0800

    feat: 新增料品偏好保存區域model
    新增料品偏好保存區域model

---
+++ src/main/java/wareHouse/Area/Model/ItemAreaRequest.java
@@ -0,0 +1,11 @@
+package wareHouse.Area.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemAreaRequest {
+    public String itemNumber;
+    public String branch;
+}
