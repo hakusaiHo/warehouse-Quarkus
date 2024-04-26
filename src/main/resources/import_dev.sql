77985de599ce87eb08d45614d78cc7c9163faffd
hakusaiHo <a88666089@gmail.com>
2024-04-26 18:36:00 +0800

    feat: 新增儲位主檔的測試資料
    新增儲位主檔的測試資料

--- src/main/resources/import_dev.sql
+++ src/main/resources/import_dev.sql
@@ -21,52 +21,115 @@ VALUES(200, '030', NOW(), 42, 'A20100', '可樂果57G24包入', '每箱24包_每
 
 INSERT INTO ktst_lot_master
 (expired_date, item_id, supplier_number, modified_date, modified_user, branch, item_number, lot_number)
-VALUES('2024-12-31', 548, 0, NOW(), 42, 'M011', '000494', '20230830');
+VALUES('2024-12-31', 548, 0, NOW(), 42, 'A011', '000494', '20230830');
 INSERT INTO ktst_lot_master
 (expired_date, item_id, supplier_number, modified_date, modified_user, branch, item_number, lot_number)
-VALUES('2024-12-31', 572, 0, NOW(), 42, 'M011', '000499', '20230830');
+VALUES('2024-12-31', 572, 0, NOW(), 42, 'A011', '000499', '20230830');
 INSERT INTO ktst_lot_master
 (expired_date, item_id, supplier_number, modified_date, modified_user, branch, item_number, lot_number)
-VALUES('2024-12-31', 570, 0, NOW(), 42, 'M011', '000495', '20230831');
+VALUES('2024-12-31', 570, 0, NOW(), 42, 'A011', '000495', '20230831');
 INSERT INTO ktst_lot_master
 (expired_date, item_id, supplier_number, modified_date, modified_user, branch, item_number, lot_number)
-VALUES('2024-12-31', 200, 0, NOW(), 42, 'M011', 'A20100', '20230832');
+VALUES('2024-12-31', 200, 0, NOW(), 42, 'A011', 'A20100', '20230832');
 INSERT INTO ktst_lot_master
 (expired_date, item_id, supplier_number, modified_date, modified_user, branch, item_number, lot_number)
-VALUES('2024-12-31', 200, 0, NOW(), 42, 'M012', 'A20100', '20230833');
+VALUES('2024-12-31', 200, 0, NOW(), 42, 'A010', 'A20100', '20230833');
 INSERT INTO ktst_lot_master
 (expired_date, item_id, supplier_number, modified_date, modified_user, branch, item_number, lot_number)
-VALUES('2024-12-31', 318, 0, NOW(), 42, 'M011', '000483', '20230831');
+VALUES('2024-12-31', 318, 0, NOW(), 42, 'A010', '000483', '20230831');
 
 INSERT INTO ktst_vehicle_master
 (pause_code, modified_date, modified_user, vehicle_type_id, branch, store_location, vehicle_id)
-VALUES('00', NOW(), 1, 'A01', 'M011', 'LW1002', 'AA0123456786');
+VALUES('00', NOW(), 1, 'SF', 'A010', 'LW1002', 'SF000001');
 INSERT INTO ktst_vehicle_master
 (pause_code, modified_date, modified_user, vehicle_type_id, branch, store_location, vehicle_id)
-VALUES('00', NOW(), 1, 'A01', 'M011', 'LW1002', 'AA0123456760');
+VALUES('00', NOW(), 1, 'PG', 'A011', 'LW1002', 'PG000001');
 
 INSERT INTO ktst_vehicle_detail
 (item_id, store_quantity, store_uom, modified_date, modified_user, lot_number, vehicle_id)
-VALUES(318, 20.0000, 'KG', NOW(), 1, '20230831', 'AA0123456780');
+VALUES(318, 20.0000, 'KG', NOW(), 1, '20230831', 'SF000001');
 INSERT INTO ktst_vehicle_detail
 (item_id, store_quantity, store_uom, modified_date, modified_user, lot_number, vehicle_id)
-VALUES(200, 20.0000, 'KG', NOW(), 1, '20230832', 'AA0123456780');
+VALUES(200, 20.0000, 'KG', NOW(), 1, '20230832', 'SF000001');
 INSERT INTO ktst_vehicle_detail
 (item_id, store_quantity, store_uom, modified_date, modified_user, lot_number, vehicle_id)
-VALUES(200, 20.0000, 'KG', NOW(), 1, '20230833', 'AA0123456780');
+VALUES(200, 20.0000, 'KG', NOW(), 1, '20230833', 'PG000001');
 INSERT INTO ktst_vehicle_detail
 (item_id, store_quantity, store_uom, modified_date, modified_user, lot_number, vehicle_id)
-VALUES(570, 40.0000, 'KG', NOW(), 1, '20230831', 'AA0123456781');
+VALUES(570, 40.0000, 'KG', NOW(), 1, '20230831', 'PG000001');
 INSERT INTO ktst_vehicle_detail
 (item_id, store_quantity, store_uom, modified_date, modified_user, lot_number, vehicle_id)
-VALUES(572, 30.0000, 'KG', NOW(), 1, '20230830', 'AA0123456782');
+VALUES(572, 30.0000, 'KG', NOW(), 1, '20230830', 'PG000001');
 
 INSERT INTO ktst_vehicle_type
 (item_id, modified_date, modified_user, vehicle_type_id, vehicle_type_spec)
-VALUES(200, NOW(), 1, 'A01', '33cm棧板');
+VALUES(200, NOW(), 1, 'SF', '田字型棧板');
 INSERT INTO ktst_vehicle_type
 (item_id, modified_date, modified_user, vehicle_type_id, vehicle_type_spec)
-VALUES(548, NOW(), 1, 'A02', '長寬50cm*50cm高30cm搬運箱');
+VALUES(548, NOW(), 1, 'PG', '長方形棧板');
 INSERT INTO ktst_vehicle_type
 (item_id, modified_date, modified_user, vehicle_type_id, vehicle_type_spec)
-VALUES(200, NOW(), 1, 'A03', '40cm棧板');
+VALUES(200, NOW(), 1, 'TH', 'H形鐵架');
+
+--3/14後更新 移儲作業
+--料品分支廠主檔
+INSERT INTO ktst_item_branch
+(item_id, lot_control, modified_date, modified_user, branch, item_number)
+VALUES(200, '', NOW(), 1, 'A010', 'A20100');
+INSERT INTO ktst_item_branch
+(item_id, lot_control, modified_date, modified_user, branch, item_number)
+VALUES(318, '', NOW(), 1, 'A011', '000483');
+INSERT INTO ktst_item_branch
+(item_id, lot_control, modified_date, modified_user, branch, item_number)
+VALUES(548, '', NOW(), 1, 'A010', '000494');
+INSERT INTO ktst_item_branch
+(item_id, lot_control, modified_date, modified_user, branch, item_number)
+VALUES(570, '', NOW(), 1, 'A010', '000495');
+INSERT INTO ktst_item_branch
+(item_id, lot_control, modified_date, modified_user, branch, item_number)
+VALUES(572, '', NOW(), 1, 'A011', '000499');
+
+--4/26 儲位主檔
+INSERT INTO ktst.ktst_location_master
+(modified_date, modified_user, branch, user_reserved_reference, location_id)
+VALUES(NOW(), 1, 'A010', '', 'V40U01');
+INSERT INTO ktst.ktst_location_master
+(modified_date, modified_user, branch, user_reserved_reference, location_id)
+VALUES(NOW(), 1, 'A010', '', 'V40U02');
+INSERT INTO ktst.ktst_location_master
+(modified_date, modified_user, branch, user_reserved_reference, location_id)
+VALUES(NOW(), 1, 'A011', '', 'V30S01');
+INSERT INTO ktst.ktst_location_master
+(modified_date, modified_user, branch, user_reserved_reference, location_id)
+VALUES(NOW(), 1, 'A011', '', 'V30T01');
+INSERT INTO ktst.ktst_location_master
+(modified_date, modified_user, branch, user_reserved_reference, location_id)
+VALUES(NOW(), 1, 'A011', '', 'V30E01');
+
+--3/18 儲區設定檔
+INSERT INTO ktst_area_info
+(modified_date, modified_user, branch, area, area_name)
+VALUES(NOW(), 1, 'A011', 'V30S', '萬歲一樓A棟東區');
+INSERT INTO ktst_area_info
+(modified_date, modified_user, branch, area, area_name)
+VALUES(NOW(), 1, 'A011', 'V30T', '萬歲一樓A棟西區');
+INSERT INTO ktst_area_info
+(modified_date, modified_user, branch, area, area_name)
+VALUES(NOW(), 1, 'A010', 'V40U', '聯華三樓U棟01區');
+--3/18 儲區儲位對照檔
+INSERT INTO ktst_area_location
+(is_virtual, modified_date, modified_user, branch, area, location_desc, location_id)
+VALUES(false, NOW(), 1, 'A011', 'V30S', '東區S1位', 'V30S01');
+INSERT INTO ktst_area_location
+(is_virtual, modified_date, modified_user, branch, area, location_desc, location_id)
+VALUES(false, NOW(), 1, 'A011', 'V30T', '東區T1位', 'V30T01');
+INSERT INTO ktst_area_location
+(is_virtual, modified_date, modified_user, branch, area, location_desc, location_id)
+VALUES(false, NOW(), 1, 'A010', 'V40U', '01區U1位', 'V40U01');
+--3/18 料品偏好保存區
+INSERT INTO ktst_item_area
+(item_id, modified_date, modified_user, branch, prefer_save_area)
+VALUES(570, NOW(), 1, 'A010', 'V20A');
+INSERT INTO ktst_item_area
+(item_id, modified_date, modified_user, branch, prefer_save_area)
+VALUES(548, NOW(), 1, 'A010', 'V30S');
