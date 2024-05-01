da6176579778784ea1778eeec9fa9108ff31befb
hakusaiHo <a88666089@gmail.com>
2024-05-01 17:38:16 +0800

    refactor: 將新增載具的豔應邏輯提取出來創建一個私有方法
    將新增載具的豔應邏輯提取出來創建一個私有方法

--- src/main/java/wareHouse/Vehicle/Controller/VehicleMasterController.java
+++ src/main/java/wareHouse/Vehicle/Controller/VehicleMasterController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
-
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import jakarta.inject.Inject;
@@ -20,24 +19,33 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import wareHouse.Master.Dao.ItemMaster.ItemMaster;
+import wareHouse.Master.Service.ItemBranchService;
 import wareHouse.Master.Service.ItemService;
 import wareHouse.Vehicle.Dao.VehicleMaster.VehicleMaster;
 import wareHouse.Vehicle.Dao.VehicleMaster.VehicleMasterPK;
 import wareHouse.Vehicle.Model.Vehicle;
 import wareHouse.Vehicle.Service.VehicleService;
-import jakarta.ws.rs.core.MediaType;
+import wareHouse.Vehicle.Service.VehicleTypeService;
 
 @Path("public/vehicle")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@Tag(name = "vehicleMaster", description = "載具主檔管理")
+@Tag(name = "VehicleMaster", description = "載具主檔管理")
 public class VehicleMasterController {
     @Inject
     VehicleService vehicleService;
 
     @Inject
     ItemService itemService;
+
+    @Inject
+    VehicleTypeService vehicleTypeService;
+
+    @Inject
+    ItemBranchService itemBranchService;
 
     @Inject
     EntityManager em;
@@ -48,28 +56,25 @@ public class VehicleMasterController {
     @Operation(summary = "新增載具主檔資訊與承載明細", description = "新增載具主檔資訊與承載明細至資料庫")
     public Response addVehicleMasterDetailByPK(Vehicle vehicle) {
 
-        if (vehicleService.existsByVehicleId(vehicle.vehicleMaster.getVehicleId())) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("此載具編號已經存在").build();
+        Response validationResponse = validateVehicleMaster(vehicle.vehicleMaster);
+        if (validationResponse != null) {
+            return validationResponse;
         }
-
-        boolean checkAllUtemId = vehicle.vehicleDetail.stream()
-                .allMatch(detail -> detail.getItemId() == null
-                        || itemService.getItemMasterByItemId(detail.getItemId()) != null);
-
-        if (!checkAllUtemId) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("輸入的短料號不存在").build();
-        }
-
         vehicle.vehicleMaster.modifiedDate = new Date();
         vehicle.vehicleMaster.modifiedUser = 1L;
         vehicle.vehicleMaster.persist();
 
         vehicle.vehicleDetail.forEach(detail -> {
+            if (detail.getItemId() != null) {
+                ItemMaster checkitem = itemService.getItemMasterByItemId(detail.getItemId());
+                if (checkitem == null) {
+                    throw new WebApplicationException(Response.Status.NOT_FOUND);
+                }
+            }
             detail.modifiedDate = new Date();
             detail.modifiedUser = 1L;
             detail.persist();
         });
-
         return Response.status(Response.Status.CREATED).entity(vehicle).build();
     }
 
@@ -79,11 +84,15 @@ public class VehicleMasterController {
     @Operation(summary = "新增載具主檔資訊", description = "新增載具主檔資訊至資料庫")
     public Response addVehicleMasterByPK(VehicleMaster vehicleMaster) {
 
-        if (vehicleService.existsByVehicleId(vehicleMaster.getVehicleId())) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("此載具編號已經存在").build();
+        Response validationResponse = validateVehicleMaster(vehicleMaster);
+        if (validationResponse != null) {
+            return validationResponse;
         }
         vehicleMaster.modifiedDate = new Date();
         vehicleMaster.modifiedUser = 1L;
+        if (vehicleMaster.getPauseCode() == null || vehicleMaster.getPauseCode().isEmpty()) {
+            vehicleMaster.setPauseCode("00");
+        }
         vehicleMaster.persist();
         return Response.created(URI.create("/vehicleMaster")).entity(vehicleMaster).build();
     }
@@ -120,5 +129,18 @@ public class VehicleMasterController {
         }
         VehicleMaster.deleteById(pk);
         return Response.status(Response.Status.OK).build();
+    }
+
+    private Response validateVehicleMaster(VehicleMaster vehicleMaster) {
+        if (vehicleService.existsByVehicleId(vehicleMaster.getVehicleId())) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        if (vehicleTypeService.findVehicleTypeList(vehicleMaster.getVehicleTypeId(), null).isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        if (itemBranchService.checkBranchExists(vehicleMaster.getBranch()) == false) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return null;
     }
 }
